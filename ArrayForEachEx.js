f89a418c18be3104885e836a7c5c59b45d85ea6f
Nirmal1992 <jainnirmal1992@gmail.com>
2020-01-03 14:05:06 +0000

    added customized for each method for array and object both

--- ArrayForEachEx.js
+++ ArrayForEachEx.js
@@ -21,3 +21,34 @@ array1.forEach(
 // expected output: "a" 0 Array ["a", "b", "c"] [object Window]
 // expected output: "b" 1 Array ["a", "b", "c"] [object Window]
 // expected output: "c" 2 Array ["a", "b", "c"] [object Window]
+
+/* 
+  customized for each method that works for both array and object
+*/
+
+const _ = {};
+_.each = function(list, callBack) {
+  if (Array.isArray(list)) {
+    list.forEach((element, index, arr) => callBack(element, index, arr));
+  } else {
+    for (const key in list) {
+      callBack(list[key], key, list);
+    }
+  }
+};
+
+const callBackFn = function(element, index, list) {
+  console.log(element, index, list);
+};
+
+_.each({ a: 'A', b: 'B', c: 'C' }, callBackFn);
+
+//expect output: 'A' 'a' { a: 'A', b: 'B', c: 'C' }
+//expect output: 'B' 'b' { a: 'A', b: 'B', c: 'C' }
+//expect output: 'C' 'c' { a: 'A', b: 'B', c: 'C' }
+
+_.each(['A', 'B', 'C'], callBackFn);
+
+//expect output:  'A' 0 [ 'A', 'B', 'C' ]
+//expect output:  'B' 1 [ 'A', 'B', 'C' ]
+//expect output:  'C' 2 [ 'A', 'B', 'C' ]
