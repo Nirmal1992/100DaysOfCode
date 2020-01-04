c4cc01b2f586b2fbc93f6ebbaacc5db3cbbcd723
Nirmal1992 <jainnirmal1992@gmail.com>
2020-01-04 03:08:58 +0000

    added map implementation

---
+++ CustomizedMapEx.js
@@ -0,0 +1,31 @@
+/* 
+  customized map method that works for both array and object
+*/
+
+const _ = {};
+_.map = function(list, callBack) {
+  const returnedList = [];
+  if (Array.isArray(list)) {
+    list.forEach((element, index, arr) => {
+      returnedList.push(callBack(element, index, arr));
+    });
+    return returnedList;
+  } else {
+    for (const key in list) {
+      returnedList.push(callBack(list[key], key, list));
+    }
+    return returnedList;
+  }
+};
+
+const callBackFn = function(element, index, arr) {
+  return `tranformed ${element}`;
+};
+
+console.log(_.map({ a: 'A', b: 'B', c: 'C' }, callBackFn));
+
+//expect output: [ 'broken A', 'broken B', 'broken C' ]
+
+console.log(_.map(['A', 'B', 'C'], callBackFn));
+
+//expect output: [ 'broken A', 'broken B', 'broken C' ]
